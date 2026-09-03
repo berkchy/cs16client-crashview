c2ffd0f5aba601fcf5f53abba39d1f89f2d71d38
pickle <pickle@local>
2026-09-03 16:54:32 +0300

    fix: pawn compiler sc3.c assertion - change =='0' (char=48) to ==0 (int) in array_level check

--- android/ci/build-amxx.sh
+++ android/ci/build-amxx.sh
@@ -97,6 +97,9 @@ apply_patch "$PATCHES/amxmodx-CTextParsers-quote-underrun.diff" "$SRC/amxmodx"
 apply_patch "$PATCHES/amxmodx-amtl-64bit.diff"             "$SRC/amxmodx" "public/amtl"
 apply_patch "$PATCHES/amxmodx-regparm-arm64.patch"         "$SRC/amxmodx"
 apply_patch "$PATCHES/amxmodx-csx-null-check.patch"        "$SRC/amxmodx"
+# Fix Pawn compiler assertion bug: =='0' (char literal = 48) should be ==0 (int zero)
+# This causes "array_level=='0'" assertion failure on any enum-constant array index
+sed -i "s/array_level==\s*'0'/array_level==0/g" "$SRC/amxmodx/compiler/libpc300/sc3.c"
 # AMXX core is still compiled against metamod-p's meta_api.h (METAMOD above),
 # which requires this ARM64 shim (cs16_amxx_compat.h + const SET_LOCALINFO).
 apply_patch "$PATCHES/metamod-p-aarch64.patch"            "$SRC/metamod-p"
