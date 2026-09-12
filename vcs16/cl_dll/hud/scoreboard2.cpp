1da86634434070cbee0c5c010126ea553197278a
pickle <pickle@local>
2026-09-12 12:03:14 +0300

    fix(scoreboard2): extern shared globals to resolve duplicate symbol errors

--- vcs16/cl_dll/hud/scoreboard2.cpp
+++ vcs16/cl_dll/hud/scoreboard2.cpp
@@ -31,20 +31,17 @@
 #include "vgui_parser.h"
 #include "eventscripts.h"
 
-hud_player_info_t   g_PlayerInfoList[MAX_PLAYERS+1]; // player info from the engine
-extra_player_info_t	g_PlayerExtraInfo[MAX_PLAYERS+1]; // additional player info sent directly to the client dll
-team_info_t         g_TeamInfo[MAX_TEAMS+1];
-hostage_info_t      g_HostageInfo[MAX_HOSTAGES+1];
-int g_iUser1;
-int g_iUser2;
-int g_iUser3;
-int g_iTeamNumber;
+extern hud_player_info_t   g_PlayerInfoList[MAX_PLAYERS+1];
+extern extra_player_info_t	g_PlayerExtraInfo[MAX_PLAYERS+1];
+extern team_info_t         g_TeamInfo[MAX_TEAMS+1];
+extern hostage_info_t      g_HostageInfo[MAX_HOSTAGES+1];
+extern int g_iUser1;
+extern int g_iUser2;
+extern int g_iUser3;
+extern int g_iTeamNumber;
 
-
-// X positions
-
-int xstart, xend;
-int ystart, yend;
+extern int xstart, xend;
+extern int ystart, yend;
 
 enum
 {
